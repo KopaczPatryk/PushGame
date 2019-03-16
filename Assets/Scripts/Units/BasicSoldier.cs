f2c6e1b779d644ed9a301e9b4200f8709b46013b
Patryk <Kopaczkapl@gmail.com>
2019-03-16 23:31:05 +0100

    Initial commit

---
+++ Assets/Scripts/Units/BasicSoldier.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicSoldier : Unit, IUnit
+{
+    public BasicSoldier(float dmg, float hp) : base(dmg, hp) { }
+
+    public void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Unit : MonoBehaviour
+    {
+        private readonly float demage;
+        private readonly float health;
+
+        public Unit(float dmg, float hp)
+        {
+            demage = dmg;
+            health = hp;
+        }
+    }
+}
