6c060f3c19a4bf6e988f8f14b183db2ae24a07a9
Patryk <Kopaczkapl@gmail.com>
2019-03-18 22:59:01 +0100

    Progress

---
+++ Assets/Scripts/Helpers/GizmosHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Helpers
+{
+    public static class GizmosHelper
+    {
+        public static void DrawWireQuad(Vector3 pos, Vector2 scale)
+        {
+            Gizmos.DrawLine(pos - (Vector3.forward * scale.y - Vector3.right * scale.x) * 0.5f, pos + (Vector3.forward * scale.y - Vector3.right * scale.x) * 0.5f);
+
+            Gizmos.DrawLine(pos - (-Vector3.forward * scale.y - Vector3.right * scale.x) * 0.5f, pos - (-Vector3.forward * scale.y + Vector3.right * scale.x) * 0.5f);
+            Gizmos.DrawLine(pos - (Vector3.forward * scale.y + Vector3.right * scale.x) * 0.5f, pos - (Vector3.forward * scale.y - Vector3.right * scale.x) * 0.5f);
+
+            Gizmos.DrawLine(pos - (Vector3.forward * scale.y - Vector3.right * scale.x) * 0.5f, pos + (Vector3.forward * scale.y + Vector3.right * scale.x) * 0.5f);
+            Gizmos.DrawLine(pos - (Vector3.forward * scale.y + Vector3.right * scale.x) * 0.5f, pos + (Vector3.forward * scale.y - Vector3.right * scale.x) * 0.5f);
+        }
+    }
+}
